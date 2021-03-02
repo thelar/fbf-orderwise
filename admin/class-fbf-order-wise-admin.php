87b208b7be6c59858e2cfff13e59ff9c3a2f7664
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2021-03-02 15:05:19 +0000

    add american express

--- admin/class-fbf-order-wise-admin.php
+++ admin/class-fbf-order-wise-admin.php
@@ -356,7 +356,12 @@ class Fbf_Order_Wise_Admin
                 $payment_method = 'Paypal';
                 break;
             case 'sagepaydirect':
-                $payment_method = 'Sage Pay Credit Card';
+                $sageresult = get_post_meta($order->get_id(),'_sageresult', true);
+                if(strtolower($sageresult['CardType'])=='amex'||strtolower($sageresult['CardType'])=='american express'){
+                    $payment_method = 'American Express';
+                }else{
+                    $payment_method = 'Sage Pay Credit Card';
+                }
                 break;
             case 'klarna_payments':
                 $payment_method = 'Klarna';
