747c9226f5a7b4ca67b959ac2a14f4ce805abe7c
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2022-05-13 11:51:57 +0100

    chore: work out fitting cost and add to line item

--- admin/class-fbf-order-wise-admin.php
+++ admin/class-fbf-order-wise-admin.php
@@ -744,13 +744,28 @@ class Fbf_Order_Wise_Admin
                         }
                         $col = $wheel_tyre_size_mapping['size_' . $fk];
                         $fitting_sku = $garage_data[$col];
+                        $price_code_cols = [
+                            88,
+                            90,
+                        ];
+                        foreach($price_code_cols as $pci => $pc){
+                            if($garage_data[$pc] == $fitting_sku){
+                                $fitting_price = $garage_data[$pc + 1];
+                                if($tax_code==='T1'){
+                                    $fitting_price_net = round($fitting_price/1.2, 2);
+                                }else{
+                                    $fitting_price_net = $fitting_price;
+                                }
+                                break;
+                            }
+                        }
                         $items['SalesOrderLine'][] = [
                             'eCommerceCode' => $fitting_sku,
                             'Code' => $fitting_sku,
                             'Quantity' => $qty,
                             'eCommerceItemID' => 'NATIONAL_FITTING_' . $fk,
-                            'ItemGross' => 0,
-                            'ItemNet' => 0,
+                            'ItemGross' => $fitting_price,
+                            'ItemNet' => $fitting_price_net,
                             'TaxCode' => $tax_code,
                             'Direct' => 'true',
                             'SelectedSupplier' => $garage_data[$garage_supplier_name_col],
